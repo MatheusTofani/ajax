ef2743ef793d7bebf29cee44de9c1f37b9c5db5a
Matheus <matheus271112@gmail.com>
2024-09-12 13:14:30 -0300

    Fim da atividade

---
+++ main.js
@@ -0,0 +1,25 @@
+$(document).ready(function () {
+
+    const avatar = $('.profile-avatar');
+    const nome = $('.profile-name');
+    const perfil = $('.profile-username');
+    const repositorios = $('.repositorios');
+    const seguidores = $('.seguidores');
+    const seguindo = $('.seguindo');
+
+    fetch('https://api.github.com/users/ogiansouza')
+        .then(function (res) {
+            return (res.json());
+        })
+
+        .then(function (json) {
+            avatar.attr('src', json.avatar_url);
+            repositorios.text(json.public_repos);
+            seguidores.text(json.followers);
+            seguindo.text(json.following);
+            nome.text(json.name);
+            perfil.text(json.login);
+        })
+})
+
+
